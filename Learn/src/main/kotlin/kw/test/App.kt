4b73edcd0f2f7c4866cc875a2bc09c4cce76a4c9
kangwang <2818815189@qq.com>
2023-11-05 15:03:14 +0800

    存储

---
+++ Learn/src/main/kotlin/kw/test/App.kt
@@ -0,0 +1,19 @@
+package kw.test
+
+//fun main(args: Array<String>) {
+//    println("Hello World!")
+//    //var表示新增一个变量
+//    //定义类型int
+//    var num:Int = 3;
+//}
+
+/**
+ * 参数可省略
+ */
+fun main(){
+    println("");
+}
+fun main(num:Array<String>){
+    println("xxxxxxxx");
+
+}
