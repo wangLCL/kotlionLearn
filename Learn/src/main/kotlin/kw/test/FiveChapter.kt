3d5384f94eb8e2c2d0acfdc16b2c6f3e160d303a
kangwang <2818815189@qq.com>
2023-11-11 19:15:22 +0800

    函数

--- Learn/src/main/kotlin/kw/test/FiveChapter.kt
+++ Learn/src/main/kotlin/kw/test/FiveChapter.kt
@@ -2,24 +2,78 @@ package kw.test
 
 class FiveChapter {
     fun test(){
-        println({
-            val current = 2018
-            "well $current"
-        }())
+        println(
+            {
+                val current = 2018
+                "well $current"
+            }()
+        )
     }
 
     fun test02(){
+        //类型
         var gen : ()->String = {
+            //string
             "xx"
         }
 
         println()
         println(gen())
     }
+
+    fun test03(){
+        var gen = {
+            println("xxx")
+        }
+
+        println(gen())
+    }
+
+    fun test04(){
+        var gen : ()-> String = {
+            ""
+        }
+
+        var gen1 : (String) -> String = { name ->
+            name
+        }
+
+        var gen2 : (String) -> String = {
+            it
+        }
+
+        var gen3 = { name : String ->
+            name
+        }
+
+    }
+
+
 }
 
 fun main() {
     var cha = FiveChapter()
+    cha.apply {
+        test()
+        test03()
+    }
     cha.test()
-    cha.test02()
+    cha.test03()
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
