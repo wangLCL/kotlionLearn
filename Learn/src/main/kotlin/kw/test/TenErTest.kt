0af684b5fa2880b4534bd6aab409363d71edff14
kangwang <2818815189@qq.com>
2023-11-20 22:38:00 +0800

    集合和类

--- Learn/src/main/kotlin/kw/test/TenErTest.kt
+++ Learn/src/main/kotlin/kw/test/TenErTest.kt
@@ -3,4 +3,48 @@ package kw.test
 //类名和文件名一样，也可以不一样  一个文件中可以定义多个类
 
 //定义构造
-class TenErTest
+class TenErTest(agnt : Boolean){
+    val agnt = agnt;
+
+    var name ="xx"
+        set(value){
+            field = value.trim()
+        }
+        get() = field.capitalize()
+
+    var age = 12
+        set(value) {
+            field = value
+        }
+        get() = field;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
