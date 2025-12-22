de25523da17c2fa8fb235e43abc51aae12a2a74a
tony <3267846129@qq.com>
2025-12-22 22:57:59 +0800

    save

--- src/main/kotlin/kw/base/part1/DemoApp1.kt
+++ src/main/kotlin/kw/base/part1/DemoApp1.kt
@@ -2,10 +2,4 @@ package org.example.kw.part1
 //
 fun main(args:Array<String>){
     println("main run ===========")
-
-    val arr = arrayOf(1,23,4,67,8,9)
-    arr.forEach {
-        aq ->
-        println(aq)
-    }
 }
